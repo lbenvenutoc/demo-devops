3034671c3c2dd27f785f72234723ab533cab8862
Luis Benvenuto <lbenvenutoc@gmail.com>
2026-04-03 08:43:26 -0500

    First commit

---
+++ src/test/java/com/example/demo/rest/HelloControllerTest.java
@@ -0,0 +1,24 @@
+package com.example.demo.rest;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+@ExtendWith(MockitoExtension.class)
+class HelloControllerTest {
+
+    private HelloController helloController;
+
+    @BeforeEach
+    public void setup() {
+        helloController = new HelloController();
+    }
+
+    @Test
+    void sayHelloTest() {
+        Assertions.assertEquals("Hello World!", helloController.sayHello());
+    }
+}
